175ab61cff309e4a9a5596ab519193cb5ae208f4
Albert Zhang <albertzhang@alberts-mbp.mynetworksettings.com>
2024-01-27 22:20:26 -0500

    cleanup

--- server/app.js
+++ server/app.js
@@ -1,28 +1,29 @@
-import express, { json } from "express";
 import { writeFile as _writeFile } from "fs";
-import cors from "cors";
 import { join } from "path";
-// import { getAudio, getAudioFile } from "./openaiService.js";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import OpenAI from "openai";
 import { resolve as _resolve } from "path";
+import OpenAI from "openai";
+import cors from "cors";
+import express from "express";
 import "dotenv/config";
 
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
+const speechFile = join(__dirname, "openai.mp3");
+
 const app = express();
 const port = 3000;
 app.use(cors());
 app.use(express.json());
 
 app.get("/file", async (req, res) => {
-  const speechFile = join(__dirname, "openai.mp3");
   const mp3 = await openai.audio.speech.create({
     model: "tts-1",
     voice: "alloy",
-    input: "The TTS model generally follows the Whisper model in terms of language support. Whisper supports the following languages and performs well despite the current voices being optimized for English:Afrikaans, Arabic, Armenian, Azerbaijani, Belarusian, Bosnian, Bulgarian, Catalan, Chinese, Croatian, Czech, Danish, Dutch, English, Estonian, Finnish, French, Galician, German, Greek, Hebrew, Hindi, Hungarian, Icelandic, Indonesian, Italian, Japanese, Kannada, Kazakh, Korean, Latvian, Lithuanian, Macedonian, Malay, Marathi, Maori, Nepali, Norwegian, Persian, Polish, Portuguese, Romanian, Russian, Serbian, Slovak, Slovenian, Spanish, Swahili, Swedish, Tagalog, Tamil, Thai, Turkish, Ukrainian, Urdu, Vietnamese, and Welsh.",
+    input:
+      "The TTS model generally follows the Whisper model in terms of language support. Whisper supports the following languages and performs well despite the current voices being optimized for English:Afrikaans, Arabic, Armenian, Azerbaijani, Belarusian, Bosnian, Bulgarian, Catalan, Chinese, Croatian, Czech, Danish, Dutch, English, Estonian, Finnish, French, Galician, German, Greek, Hebrew, Hindi, Hungarian, Icelandic, Indonesian, Italian, Japanese, Kannada, Kazakh, Korean, Latvian, Lithuanian, Macedonian, Malay, Marathi, Maori, Nepali, Norwegian, Persian, Polish, Portuguese, Romanian, Russian, Serbian, Slovak, Slovenian, Spanish, Swahili, Swedish, Tagalog, Tamil, Thai, Turkish, Ukrainian, Urdu, Vietnamese, and Welsh.",
   });
   const buffer = Buffer.from(await mp3.arrayBuffer());
   await fs.promises.writeFile(speechFile, buffer);
@@ -35,7 +36,6 @@ app.get("/file", async (req, res) => {
 });
 
 app.get("/stream", async (req, res) => {
-  const speechFile = join(__dirname, "openai.mp3");
   const response = await openai.audio.speech.create({
     model: "tts-1",
     voice: "alloy",
@@ -51,23 +51,6 @@ app.get("/stream", async (req, res) => {
   stream.on("end", res.end);
 });
 
-app.get("/openai", async (req, res) => {
-  const mp3Path = join(__dirname, "openai.mp3");
-  const body = await getAudio(mp3Path);
-  console.log(body);
-  res.send(body);
-  // res.sendFile(mp3Path, {
-  //   mimetype: "audio/mpeg",
-  //   lastModified: false,
-  //   headers: false,
-  // });
-});
-
-app.post("/voice", (req, res) => {
-  const { text } = req.body;
-  console.log(text);
-  res.send(text);
-});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
