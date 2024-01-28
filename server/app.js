63cecb3004340dd5a79edcd6d82cae8b0c5e4c3a
Albert Zhang <albertzhang@alberts-mbp.mynetworksettings.com>
2024-01-27 20:13:34 -0500

    streaming

--- server/app.js
+++ server/app.js
@@ -1,12 +1,13 @@
 import { TextToSpeechClient } from "@google-cloud/text-to-speech";
 import express, { json } from "express";
-import { writeFile as _writeFile } from "fs";
+import { writeFile as _writeFile, createReadStream } from "fs";
 import util from "util";
 import cors from "cors";
 import { join } from "path";
 import { getAudio, getAudioFile } from "./openaiService.js";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import byline from "byline";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
@@ -24,6 +25,18 @@ app.get("/", async (req, res) => {
     headers: false,
   });
 });
+
+app.get('/stream', async (req, res) => {
+  const speechFile = join(__dirname, "openai.mp3");
+  // write to file as stream
+  await getAudio(speechFile);
+
+  // read from file as stream
+  let stream = createReadStream(speechFile);
+  stream.pipe(res);
+  stream.on('end', res.end);
+});
+
 
 app.get("/openai", async (req, res) => {
   const mp3Path = join(__dirname, "openai.mp3");
